2a17f03a210235fa8eea203072c38f147abbc367
Eremeow138 <eremeow138@yandex.ru>
2021-02-15 17:20:54 +0800

    feat: add audio playback and changing the style of keys on keypress

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const piano = document.querySelector('.piano');
 
 const pianoKeys = document.querySelectorAll('.piano-key');
 
+let isPressed = false;
+
 function playAudio(src) {
   auduo.src = src;
   auduo.currentTime = 0;
@@ -29,4 +31,31 @@ piano.addEventListener('mouseup', (event) => {
   if (event.target.classList.contains('piano-key')) {
     event.target.classList.remove('piano-key-active');
   }
+});
+
+window.addEventListener('keydown', (event) => {
+  if (isPressed) {
+    return false;
+  }
+  isPressed = true;
+  pianoKeys.forEach((el) => {
+    if (el.classList.contains('piano-key-active')) {
+      el.classList.remove('piano-key-active');
+    }
+    if (event.code.slice(-1) === el.dataset.letter) {
+      const note = el.dataset.note;
+      const src = `assets/audio-mp3/${note}.mp3`;
+      playAudio(src);
+      el.classList.add('piano-key-active');
+    }
+  });
+});
+
+window.addEventListener('keyup', (event) => {
+  pianoKeys.forEach((el) => {
+    if (event.code.slice(-1) === el.dataset.letter) {
+      el.classList.remove('piano-key-active');
+      isPressed = false;
+    }
+  });
 });
